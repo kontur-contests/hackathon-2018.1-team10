9f7e80ec300bb3d2b4eabf341f214603ab587443
nick0124 <nick0124creator@gmail.com>
2018-04-07 20:40:24 +0500

    Fix players

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -5,11 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour {
 
-    public string deathSceneName;
+    public Transform player1Spawn;
+    public Transform player2Spawn;
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Player")
-            SceneManager.LoadScene(deathSceneName);
+        if (coll.gameObject.name == "Player1")
+            coll.gameObject.transform.position = player1Spawn.position;
+
+        if (coll.gameObject.name == "Player2")
+            coll.gameObject.transform.position = player2Spawn.position;
     }
 }
