5d75470b7d1da2074b50182cd760ae73ab9c0ea6
nick0124 <nick0124creator@gmail.com>
2018-04-07 13:41:05 +0500

    Create cam controller

---
+++ Assets/Scripts/CamPosCalculation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPosCalculation : MonoBehaviour {
+
+    public Transform player1Pos;
+    public Transform player2Pos;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+
+        transform.position = new Vector3((player1Pos.position.x + player2Pos.position.x) / 2, (player1Pos.position.y + player2Pos.position.y) / 2, (player1Pos.position.z + player2Pos.position.z) / 2);
+    }
+}
