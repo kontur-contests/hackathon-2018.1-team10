95d80f13786a1148a6d9ea537578f801c0657eb0
nick0124 <nick0124creator@gmail.com>
2018-04-07 19:05:20 +0500

    Create level and coin effect

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Coin : MonoBehaviour {
+
+    public string player1WinSceneName;
+    public string player2WinSceneName;
 
 	// Use this for initialization
 	void Start () {
@@ -16,7 +20,10 @@ public class Coin : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.gameObject.tag == "Player")
-            Destroy(gameObject);
+        if (coll.gameObject.tag == "Player1")
+            SceneManager.LoadScene(player1WinSceneName);
+
+        if (coll.gameObject.tag == "Player2")
+            SceneManager.LoadScene(player2WinSceneName);
     }
 }
