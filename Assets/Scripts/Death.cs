3d9e9001ad5527ef054e239a59f0ba8b1310c25e
nick0124 <nick0124creator@gmail.com>
2018-04-08 05:28:44 +0500

    Create player death effect

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 public class Death : MonoBehaviour {
 
     public Transform playerSpawn;
+    public GameObject explosionPrefub;
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Killer")
+        {
+            GameObject go = (GameObject)Instantiate(explosionPrefub, gameObject.transform.position, gameObject.transform.rotation);
             gameObject.transform.position = playerSpawn.position;
+        }
     }
 }
